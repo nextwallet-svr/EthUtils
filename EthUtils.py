8674f8057afafd7cff9e07b1fe793c54dc7b6d2a
rockercheng <8350088@qq.com>
2018-10-26 13:58:28 +0800

    add get_block_with_retry_poa

--- EthUtils.py
+++ EthUtils.py
@@ -173,6 +173,20 @@ def get_block_with_retry(block_num_or_hash, max_retry=3, interval=0.5):
         debug('getBlock: e: %s, block_num_or_hash: %s', e, block_num_or_hash)
     return block
 
+def get_block_with_retry_poa(block_num_or_hash, max_retry=3, interval=0.5):
+    block = None
+    try:
+        block = getWeb3Poa().eth.getBlock(block_num_or_hash)
+        while block is None and max_retry > 0:
+            error("getblock %s failed, retry after %s sec, max retry %s" %
+                  (block_num_or_hash, interval, max_retry))
+            time.sleep(interval)
+            max_retry -= 1
+            block = getWeb3Poa().eth.getBlock(block_num_or_hash)
+    except (Exception) as e:
+        debug('getBlock: e: %s, block_num_or_hash: %s', e, block_num_or_hash)
+    return block
+
 g_filter_map = {}
 
 def destroyFilter(filter):
