cb87ba43218e7f94dba9fe7c770ca996cb324084
rockercheng <8350088@qq.com>
2019-01-18 17:11:05 +0800

    add infura w3

--- Web3Utils.py
+++ Web3Utils.py
@@ -3,6 +3,7 @@ from web3 import Web3, HTTPProvider
 from ServerConfig import *
 from LogUtil import *
 from web3.middleware import geth_poa_middleware
+from web3.auto.infura import w3
 
 # web3 provider 最大重连次数
 MAX_WEB3_AUTO_RECONNECT_ATTEMPTS = 5
@@ -12,30 +13,33 @@ web3 = None
 def getWeb3():
     global web3
 
-    if (not (web3 is None)):
-        is_connected = web3.isConnected()
-        if is_connected:
-            return web3
-
-    for attempt in range(MAX_WEB3_AUTO_RECONNECT_ATTEMPTS):
-        try:
-            web3 = Web3(HTTPProvider(GET_GETH_CONNECT_URL(), request_kwargs={'timeout': 15}))
-            if web3 is None:
-                error('getWeb3 none, try connect web3 times: %d', attempt + 1)
-                raise(Exception('try connect web3 none'))
-
+    if INFURA_API_SWITCH_ON:
+        web3 = w3
+    else:
+        if (not (web3 is None)):
             is_connected = web3.isConnected()
             if is_connected:
                 return web3
-            else:
-                error('getWeb3 not connected, try connect web3 times: %d', attempt + 1)
-                raise(Exception('try connect web3 fail'))
-        except (Exception) as e:
-            wait_t = 0.5 * pow(2, attempt)
-            error('getWeb3 fail attempt: %d, e: %s, wait_t: %.1f', attempt, e, wait_t)
-            time.sleep(wait_t)
 
-    fatal('web3 reconnect attempt totally fail, attempt: %d', attempt)
+        for attempt in range(MAX_WEB3_AUTO_RECONNECT_ATTEMPTS):
+            try:
+                web3 = Web3(HTTPProvider(GET_GETH_CONNECT_URL(), request_kwargs={'timeout': 15}))
+                if web3 is None:
+                    error('getWeb3 none, try connect web3 times: %d', attempt + 1)
+                    raise(Exception('try connect web3 none'))
+
+                is_connected = web3.isConnected()
+                if is_connected:
+                    return web3
+                else:
+                    error('getWeb3 not connected, try connect web3 times: %d', attempt + 1)
+                    raise(Exception('try connect web3 fail'))
+            except (Exception) as e:
+                wait_t = 0.5 * pow(2, attempt)
+                error('getWeb3 fail attempt: %d, e: %s, wait_t: %.1f', attempt, e, wait_t)
+                time.sleep(wait_t)
+
+        fatal('web3 reconnect attempt totally fail, attempt: %d', attempt)
     return web3
 
 def getWeb3Poa(_geth_url = None):
@@ -67,3 +71,4 @@ def getWeb3Poa(_geth_url = None):
 
     fatal('web3 reconnect attempt totally fail, attempt: %d', attempt)
     return _web3
+
